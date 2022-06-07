d792e24e0c90cd39da983d6c7360cb131852e14c
eduardkb <eduardbuhali@gmail.com>
2022-06-07 14:49:42 -0300

    first secondary commit test

--- 10-Common_Character_Count.py
+++ 10-Common_Character_Count.py
@@ -4,6 +4,7 @@ Ex:
     For s1 = "aabcc" and s2 = "adcaa", the output should be
     solution(s1, s2) = 3.
     Strings have 3 common characters - 2 "a"s and 1 "c".
+    
 """
 
 from MyModule import fClearConsole, fStopWatch
